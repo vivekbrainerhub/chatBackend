759e15842f95d749f46b13221d018c3fac64bacc
Kumar Gaurav <kumardeveloper80@gmail.com>
2025-01-07 12:22:55 +0530

    done

--- database/db.js
+++ database/db.js
@@ -2,11 +2,16 @@ const mongoose = require("mongoose");
 const mongoString = process.env.DATABASE_URL;
 
 exports.connectDB = async () => {
+  if (!mongoString) {
+    console.error("DATABASE_URL is not defined in the environment variables.");
+    process.exit(1); // Exit with failure code
+  }
+
   try {
     const conn = await mongoose.connect(mongoString);
-    console.log("Mongoose DB Connected");
+    console.log(`Mongoose DB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log("error",error)
+    console.error("Error connecting to MongoDB:", error.message);
     process.exit(1);
   }
 };
